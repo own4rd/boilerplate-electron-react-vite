c8a2ecff09bc34f002c5aecd41d7000fdd379f5c
Wagner Palmeira <wagner.araujo@itec.al.gov.br>
2025-12-10 10:34:32 -0300

    Feat: Adding initial boiler to vite+react+electron

---
+++ electron/main.js
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, '../electron/preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+
+    // TODO Adicionar .env e verificar se está ativo o debug
+    // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    //additional logic here
+}).then(createWindow)
+
+app.on('window-all-closed', () => {
+    // eslint-disable-next-line no-undef
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
